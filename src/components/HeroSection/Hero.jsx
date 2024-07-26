36dd89f55945530daa18213e978f6be95bcee0e5
onisEg <anasabdo704@gmail.com>
2024-07-26 05:48:14 +0400

    edit hero seciton

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -5,24 +5,20 @@ export default function Hero() {
   return (
     <>
       <div className="sec-1 d-flex justify-content-between align-items-center py-5 row">
-        <div className="col-md-6">
-          <article>
-            <h1>
-              Find, book and rent a car <span>Easily</span>
-            </h1>
-            <p>
-              Get a car wherever and whenever you need it with your IOS and
-              Android device.
-            </p>
-            <div className="d-flex ">
-              <img className="img-fluid " src="/google_play.png" alt="" />
-              <img className=" mx-3" src="/app_store.png" alt="" />
-            </div>
-          </article>
-        </div>
-        <div className="col-md-6">
-          <img className="" src="/carHome.png" alt="" />
-        </div>
+        <article>
+          <h1>
+            Find, book and rent a car <span>Easily</span>
+          </h1>
+          <p>
+            Get a car wherever and whenever you need it with your IOS and
+            Android device.
+          </p>
+          <div className="d-flex ">
+            <img className="img-fluid " src="/google_play.png" alt="" />
+            <img className=" mx-3" src="/app_store.png" alt="" />
+          </div>
+        </article>
+        <img className="" src="/carHome.png" alt="" />
       </div>
     </>
   );
